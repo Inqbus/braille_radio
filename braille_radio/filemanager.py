0217b213d1ed501c07fc532f89c77b4b810934d2
volker <volker.jaenisch@inqbus.de>
2024-04-29 01:24:19 +0200

    Going up keeps selected directory

--- braille_radio/filemanager.py
+++ braille_radio/filemanager.py
@@ -29,10 +29,11 @@ class FileManagerStart(Screen):
     """
     def __init__(self, parent, screen=None):
         super(FileManagerStart, self).__init__(parent, screen=screen)
+        self.show_dot = True
         self.wd = Path(os.getcwd())
         self.depth = len(self.wd.parts)
-        self.local = []
-        self.local_index = 0
+        self.current_dir = []
+        self.current_dir_index = 0
         self.scan()
 
     def init_key_handler(self):
@@ -40,41 +41,56 @@ class FileManagerStart(Screen):
         self.key_handler[KEY_ENTER] = self.down
         self.key_handler['\n'] = self.down
         self.key_handler['KEY_LEFT'] = self.up
+        self.key_handler['.'] = self.toggle_dot
+
+    def toggle_dot(self):
+        self.show_dot = not self.show_dot
+        self.scan()
+        self.render()
 
     def down(self):
-        if self.local[self.local_index][1]:
+        if self.current_dir[self.current_dir_index][1]:
             self.depth += 1
-            self.wd = self.wd / self.local[self.local_index][0]
+            self.wd = self.wd / self.current_dir[self.current_dir_index][0]
+            self.current_dir_index = 0
             self.scan()
             self.render()
 
     def up(self):
         self.depth += 1
+        old_wd = self.wd
         self.wd = self.wd.parent
         self.scan()
+        # find the subdir where we have come from
+        self.current_dir_index = self.current_dir.index((old_wd.name, old_wd.is_dir()))
         self.render()
 
     def scan(self):
-        files = [(i.name, i.is_dir()) for i in self.wd.glob('*')]
-        files = sorted(files, key=lambda file: (not file[1], file[0].lower()))
-        self.local = files
+        files = []
+        for i in self.wd.glob('*'):
+            if not self.show_dot and i.name.startswith('.'):
+                continue
+            files.append((i.name, i.is_dir()))
 
-        self.local_index = 0
+        files = sorted(files, key=lambda file: (not file[1], file[0].lower()))
+        self.current_dir = files
+        if len(self.current_dir) == 0:
+            self.current_dir = [('|EMPTY|', False)]
 
     def payload(self):
         left_part = f"{'/'.join(self.wd.parts[-3:])}"
-        if self.local[self.local_index][1]:
-            middle_part = f'<{self.local[self.local_index][0]}>'
+        if self.current_dir[self.current_dir_index][1]:
+            middle_part = f'<{self.current_dir[self.current_dir_index][0]}>'
         else:
-            middle_part = f'{self.local[self.local_index][0]}'
+            middle_part = f'{self.current_dir[self.current_dir_index][0]}'
         self.screen.addstr(left_part + ' ' + middle_part)
 
     def cursor_up(self):
-        if self.local_index > 0:
-            self.local_index -= 1
+        if self.current_dir_index > 0:
+            self.current_dir_index -= 1
         self.render()
 
     def cursor_down(self):
-        if self.local_index < len(self.local) - 1:
-            self.local_index += 1
+        if self.current_dir_index < len(self.current_dir) - 1:
+            self.current_dir_index += 1
         self.render()
